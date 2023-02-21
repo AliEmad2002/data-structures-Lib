cabb3aef50ef5245463e72e12da2be03b05945ac
AliEmad2002 <99054912+AliEmad2002@users.noreply.github.com>
2023-02-21 22:49:40 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,111 +1,111 @@
-#include "Std_Types.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "print.h"
-#include <time.h>
-
-#include "Linked_List.h"
-
-#define INITIAL_NUMBER_OF_LINKS		10
-#define MAX_DATA					1000
-//#define RND_INIT					571468
-
-
-b8 is1Smaller	(const s32* n1Ptr, const s32* n2Ptr);
-b8 is1Larger	(const s32* n1Ptr, const s32* n2Ptr);
-b8 areEqual		(const s32* n1Ptr, const s32* n2Ptr);
-void copyData	(s32* distPtr, const s32* srcPtr);
-
-void printData	(const s32* dataPtr);
-
-void init_list(LinkedList_t* l);
-
-int main(void) {
-	/*	Init emulated random number generation	*/
-	time_t tStart;
-	srand(time(&tStart));
-
-	/*	Init linked list	*/
-	LinkedList_t l;
-	init_list(&l);
-
-	/*	add random data to list	*/
-	for (u8 i = 0; i < INITIAL_NUMBER_OF_LINKS; i++)
-	{
-		s32 newData = rand() % MAX_DATA;
-		LinkedList_voidAddFirst(&l, &newData);
-	}
-
-	LinkedList_voidPrintList(&l);
-
-	LinkedList_Link_t* second = l.first->next;
-
-	LinkedList_Link_t* third = second->next;
-
-	LinkedList_Link_t** current1NextPP = &(second->next);
-
-	LinkedList_Link_t** current2NextPP = &(third->next);
-
-	LinkedList_Link_t** prev1NextPP = &(l.first->next);
-
-	LinkedList_Link_t** prev2NextPP = &(second->next);
-
-	LinkedList_Link_t* temp = *prev1NextPP;
-
-	*prev1NextPP = *prev2NextPP;
-	*prev2NextPP = temp;
-
-	temp = *current1NextPP;
-
-	*current1NextPP = *current2NextPP;
-
-	*current2NextPP = temp;
-
-
-
-
-	//LinkedList_voidSortAscending(&l);
-
-	LinkedList_voidPrintList(&l);
-
-	while(1);
-
-	return 0;
-}
-
-
-
-b8 is1Smaller	(const s32* n1Ptr, const s32* n2Ptr)
-{
-	return (*n1Ptr < *n2Ptr);
-}
-
-b8 is1Larger	(const s32* n1Ptr, const s32* n2Ptr)
-{
-	return (*n1Ptr > *n2Ptr);
-}
-
-b8 areEqual		(const s32* n1Ptr, const s32* n2Ptr)
-{
-	return (*n1Ptr == *n2Ptr);
-}
-
-void copyData	(s32* distPtr, const s32* srcPtr)
-{
-	*distPtr = *srcPtr;
-}
-
-void printData	(const s32* dataPtr)
-{
-	PRINTF("%d", *dataPtr);
-}
-
-void init_list(LinkedList_t* l)
-{
-	LinkedList_voidInit(
-		l,
-		NULL,
-		is1Smaller, is1Larger, areEqual, copyData
-	);
-}
-
+//#include "Std_Types.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "print.h"
+//#include <time.h>
+//
+//#include "Linked_List.h"
+//
+//#define INITIAL_NUMBER_OF_LINKS		10
+//#define MAX_DATA					1000
+////#define RND_INIT					571468
+//
+//
+//b8 is1Smaller	(const s32* n1Ptr, const s32* n2Ptr);
+//b8 is1Larger	(const s32* n1Ptr, const s32* n2Ptr);
+//b8 areEqual		(const s32* n1Ptr, const s32* n2Ptr);
+//void copyData	(s32* distPtr, const s32* srcPtr);
+//
+//void printData	(const s32* dataPtr);
+//
+//void init_list(LinkedList_t* l);
+//
+//int main(void) {
+//	/*	Init emulated random number generation	*/
+//	time_t tStart;
+//	srand(time(&tStart));
+//
+//	/*	Init linked list	*/
+//	LinkedList_t l;
+//	init_list(&l);
+//
+//	/*	add random data to list	*/
+//	for (u8 i = 0; i < INITIAL_NUMBER_OF_LINKS; i++)
+//	{
+//		s32 newData = rand() % MAX_DATA;
+//		LinkedList_voidAddFirst(&l, &newData);
+//	}
+//
+//	LinkedList_voidPrintList(&l);
+//
+//	LinkedList_Link_t* second = l.first->next;
+//
+//	LinkedList_Link_t* third = second->next;
+//
+//	LinkedList_Link_t** current1NextPP = &(second->next);
+//
+//	LinkedList_Link_t** current2NextPP = &(third->next);
+//
+//	LinkedList_Link_t** prev1NextPP = &(l.first->next);
+//
+//	LinkedList_Link_t** prev2NextPP = &(second->next);
+//
+//	LinkedList_Link_t* temp = *prev1NextPP;
+//
+//	*prev1NextPP = *prev2NextPP;
+//	*prev2NextPP = temp;
+//
+//	temp = *current1NextPP;
+//
+//	*current1NextPP = *current2NextPP;
+//
+//	*current2NextPP = temp;
+//
+//
+//
+//
+//	//LinkedList_voidSortAscending(&l);
+//
+//	LinkedList_voidPrintList(&l);
+//
+//	while(1);
+//
+//	return 0;
+//}
+//
+//
+//
+//b8 is1Smaller	(const s32* n1Ptr, const s32* n2Ptr)
+//{
+//	return (*n1Ptr < *n2Ptr);
+//}
+//
+//b8 is1Larger	(const s32* n1Ptr, const s32* n2Ptr)
+//{
+//	return (*n1Ptr > *n2Ptr);
+//}
+//
+//b8 areEqual		(const s32* n1Ptr, const s32* n2Ptr)
+//{
+//	return (*n1Ptr == *n2Ptr);
+//}
+//
+//void copyData	(s32* distPtr, const s32* srcPtr)
+//{
+//	*distPtr = *srcPtr;
+//}
+//
+//void printData	(const s32* dataPtr)
+//{
+//	PRINTF("%d", *dataPtr);
+//}
+//
+//void init_list(LinkedList_t* l)
+//{
+//	LinkedList_voidInit(
+//		l,
+//		NULL,
+//		is1Smaller, is1Larger, areEqual, copyData
+//	);
+//}
+//
